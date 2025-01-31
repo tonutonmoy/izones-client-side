436c181ab2efeb7001ebc6f8d81e007772a9fb0a
tonutonmoy <www.tonutonmoy12@gmail.com>
2025-01-31 16:08:45 +0600

    done

--- src/SharedComponent/Question/Question.tsx
+++ src/SharedComponent/Question/Question.tsx
@@ -4,11 +4,18 @@ import { useState } from 'react';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
 const questionsData = [
-  { question: "What is iZones?", answer: "Yes. It adheres to the WAI-ARIA design pattern.    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum suscipit hic, aperiam, a ab commodi, iste placeat sequi doloremque repudiandae quidem dicta perferendis recusandae. Non totam aperiam perferendis maiores minus?" },
-  { question: "What is our goal?", answer: "Yes. It comes with default styles that matches the other components' aesthetic.    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum suscipit hic, aperiam, a ab commodi, iste placeat sequi doloremque repudiandae quidem dicta perferendis recusandae. Non totam aperiam perferendis maiores minus?" },
-  { question: "What is our service?", answer: "Yes. It's animated by default, but you can disable it if you prefer.    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum suscipit hic, aperiam, a ab commodi, iste placeat sequi doloremque repudiandae quidem dicta perferendis recusandae. Non totam aperiam perferendis maiores minus?" },
-  { question: "Why you will trust us?", answer: "Yes. It's animated by default, but you can disable it if you prefer.    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum suscipit hic, aperiam, a ab commodi, iste placeat sequi doloremque repudiandae quidem dicta perferendis recusandae. Non totam aperiam perferendis maiores minus?" },
-  { question: "How do we solve your problem?", answer: "Yes. It's animated by default, but you can disable it if you prefer.    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum suscipit hic, aperiam, a ab commodi, iste placeat sequi doloremque repudiandae quidem dicta perferendis recusandae. Non totam aperiam perferendis maiores minus?" },
+  { question: "What is iZones?", answer: "iZones is a colour imaginary umbrella to shed you with service in your day-to-day lives " },
+  { question: "What is our goal?", answer: "Let's Connect worldwide and help each other shine" },
+  { question: "What services do we offer?", answer: `1.
+We offer multiple educational courses online such as IELTS, GRE & TOEFL, CELPIP, ESL, Duo Lingo, English basics and so on
+
+2. We offer Technical Support for any of your Apple devices over the phone. We are also specialized in resolving Apple Account issues` },
+  { question: "Why trust us?", answer: `Because we say NO!
+
+We are committed to provide you with what you deserve. We only offer services that we have skilled professionals for. If we cannot get your job done, we say NO` },
+  { question: "What makes us special?", answer: `15 minutes make us special
+Regardless of wanting to book IELTS courses with us, or making online appointments with us to get your Apple Account fixed, you can always schedule a 15 minutes online consultation with our experts and it’s absolutely FREE! ` },
+  { question: "How to get our services?", answer: `Please send us a message on WhatsApp and we will send you a Google Form to fill out. Once we receive the forms, our services will start knocking on your doors` },
 ];
 
 function Question() {
